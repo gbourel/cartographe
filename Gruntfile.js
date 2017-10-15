8296f69f19675a5cc822f2fbf512123f3f6aac6f
Guillaume Bourel <gbourel@gmail.com>
2017-10-15 21:46:47 +0300

    Init Gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,159 @@
+module.exports = function(grunt){
+  const devPort        = 9200,
+    connectPort        = 9287;
+
+  require('load-grunt-tasks')(grunt);
+
+  var baseUrl = 'https://gbourel.github.io/cartographe';
+    md5Filenames = {};
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      files: [
+      ]
+    },
+    less: {
+      default: {
+        options: {
+          banner: '/* GENERATED FROM LESS */\n',
+          sourceMap: true,
+          ieCompat: true
+        },
+        files: [{
+          'public/css/cards.css': ['less/cards.less']
+        }]
+      }
+    },
+    cssmin: {
+      options: {
+      },
+      target: {
+        files: {
+          'docs/css/cards.css': ['public/css/cards.css']
+        }
+      }
+    },
+    connect: {
+      options: {
+        port: connectPort,
+        hostname: 'localhost'
+      },
+      local: {
+        options: {
+          base: ['public'],
+          port: 9200,
+          open: true,
+          livereload: 29976
+        }
+      }
+    },
+    copy: {
+      config_dev: {
+        src: 'js/dev-constants.js', dest: 'public/js/constants.js'
+      },
+      dev: {
+        src: 'index.html', dest: 'public/index.html',
+        options: {
+          process: function (content) {
+            content = content.replace(/%BASE_PATH%/g, '');
+            content = content.replace(/%DEPENDENCIES%/g, '<script src="config.js"></script>');
+            return content;
+          }
+        }
+      },
+      config_dist: {
+        src: 'js/prod-constants.js', dest: 'public/js/constants.js'
+      },
+      dist: {
+        files: [{
+          expand: true, dot: true,
+          cwd: 'public', dest: 'docs',
+          src: [
+            'css/font-awesome.min.css',
+            'css/cards.css',
+            'img/**/*',
+            'favicon.ico',
+            'lib/system.js'
+          ]
+        }]
+      },
+      distIndex: {
+        src: 'index.html', dest: 'docs/index.html',
+        options: {
+          process: function (content) {
+            content = content.replace(/%BASE_PATH%/g, baseUrl);
+            content = content.replace(/%DEPENDENCIES%/g,
+                '<script src="' + baseUrl + '/js/'+ md5Filenames['libs.js'] +'"></script>\n' +
+                '    <script src="' + baseUrl + '/js/' + md5Filenames['cards.js'] + '"></script>');
+            return content;
+          }
+        }
+      }
+    },
+    md5: {
+      bundle: {
+        files: {
+          'docs/js/': 'tmp/*.js'
+        },
+        options: {
+          keepBasename: true,
+          keepExtension: true,
+          afterEach: function (fileChange) {
+            var oldName = fileChange.oldPath.substring(fileChange.oldPath.lastIndexOf('/')+1);
+            var newName = fileChange.newPath.substring(fileChange.newPath.lastIndexOf('/')+1);
+            md5Filenames[oldName] = newName;
+          }
+        }
+      }
+    },
+    shell: {
+      bundle: {
+        command: [
+          'jspm bundle js/cards.js - jquery - moment tmp/cards.js --minify --skip-source-maps',
+          'jspm bundle jquery + moment tmp/libs.js --minify --skip-source-maps'
+        ].join('&&')
+      }
+    },
+    usage: {
+      options: {
+        'title': 'Cartographe\n',
+        'taskGroups': [{
+          'header': 'Release tasks',
+          'tasks': ['release']
+        },{
+          'header': 'Dev tasks',
+          'tasks': ['dev']
+        }],
+        'taskDescriptionOverrides': {
+          'dev': 'Runs livereload development server.'
+        }
+      }
+    },
+    watch: {
+      options: {
+        livereload: 29976
+      },
+      html: {
+        files: ['index.html'],
+        tasks: ['copy:dev']
+      },
+      i18n: {
+        files: ['public/i18n/*.js'],
+        tasks: []
+      },
+      js: {
+        files: ['public/js/**/*.js'],
+        tasks: []
+      },
+      css: {
+        files: ['less/*.less'],
+        tasks: ['less']
+      }
+    }
+  });
+
+  grunt.registerTask('release', ['less', 'cssmin', 'copy:config_dist', 'shell:bundle', 'copy:dist', 'md5', 'copy:distIndex']);
+  grunt.registerTask('dev', ['less', 'copy:config_dev', 'copy:dev', 'connect:local', 'watch']);
+  grunt.registerTask('default', ['usage']);
+};
